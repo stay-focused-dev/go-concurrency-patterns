2d492e8e663b9abf7ef587e38a365a6d775147fb
stay-focused-dev <maxim@stay-focused.dev>
2025-11-07 15:52:13 +0300

    generator pattern

--- boring.go
+++ boring.go
@@ -6,18 +6,23 @@ import (
 	"time"
 )
 
-func boring(msg string, c chan string) {
-	for i := 0; ; i++ {
-		c <- fmt.Sprintf("%s %d", msg, i)
-		time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
-	}
+func boring(msg string) <-chan string {
+	c := make(chan string)
+	go func() {
+		for i := 0; ; i++ {
+			c <- fmt.Sprintf("%s %d", msg, i)
+			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+		}
+	}()
+	return c
 }
 
 func main() {
-	c := make(chan string)
-	go boring("boring!", c)
+	joe := boring("Joe")
+	ann := boring("Ann")
 	for i := 0; i < 5; i++ {
-		fmt.Printf("You say: %q\n", <-c)
+		fmt.Println(<-joe)
+		fmt.Println(<-ann)
 	}
 	fmt.Println("You're boring; I'm leaving.")
 }
